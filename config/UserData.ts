fd4f0939578d534530e335d2520628158658d201
Dhrutinandan Swain <dhrutiswainnandan@gmail.com>
2024-10-12 20:43:36 +0530

    feat: Initial project setup

---
+++ config/UserData.ts
@@ -0,0 +1,15 @@
+interface Profile {
+  id: number;
+  name: string;
+  username: string;
+  bio: string;
+}
+
+export const profiles: Profile[] = [
+  {
+    id: 1,
+    name: 'Dhrutinandan Swain',
+    username: 'swaindhruti',
+    bio: 'Full-stack & Web3 Developer',
+  },
+];
